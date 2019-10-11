2475e375b094628d59131b3a6bc1a3355aeb9baf
Josep Maria Lozano <josepmaria.aguilar@sage.com>
2019-10-11 11:48:34 +0200

    WIP

--- Server/Host/Controllers/TrainingController.cs
+++ Server/Host/Controllers/TrainingController.cs
@@ -10,33 +10,32 @@
 
         // GET api/Training
         [HttpGet]
-        public ActionResult<IEnumerable<string>> GetAll([FromQuery]Guid ObjectiveId, [FromQuery]int ItemsPerPage, [FromQuery]int Page) {
-            return new string[] { "Training Get All" };
+        public ActionResult<List<Training>> GetAll([FromQuery]Guid objectiveId, [FromQuery]int itemsPerPage, [FromQuery]int page) {
+            return null;
         }
 
         // GET api/Training/5
         [HttpGet("{Id}")]
-        public ActionResult<string> Get(Guid Id) {
-            return "Training Get by Id";
+        public ActionResult<Training> Get(Guid id) {
+            return null;
         }
 
         // POST api/Training
         [HttpPost]
-        public ActionResult<string> Add([FromBody]Training Training) {
-            return "Training Post";
+        public ActionResult<Guid> Add([FromBody]Training training) {
+            return null;
         }
 
         // PUT api/Training/5
         [HttpPut("{Id}")]
-        public ActionResult<string> Update(Guid Id, [FromBody]Training Training) {
-            return "Training Put";
+        public ActionResult<Training> Update(Guid id, [FromBody]Training training) {
+            return null;
         }
 
         // DELETE api/Training/5
         [HttpDelete("{Id}")]
-        public ActionResult<string> Delete(Guid Id) {
-            // async Task<IActionResult>
-            return "Training Delete";
+        public void Delete(Guid id) {
+            
         }
     }
 }
